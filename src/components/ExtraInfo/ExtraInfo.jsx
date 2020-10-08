1fbe90991f25c162bfe3cddcb55b63dde26bc8ce
priyansh <singhalpriyansh58@gmail.com>
2020-10-08 18:29:00 +0530

    Fixed errors

--- src/components/ExtraInfo/ExtraInfo.jsx
+++ src/components/ExtraInfo/ExtraInfo.jsx
@@ -10,7 +10,7 @@ const ExtraInfo = () => {
         </h3>
         <p className="break-words">I'm a Full Stack Developer, I'm a quick learner and can transform ideas into webpages.</p>
         <p className="break-words">
-         Currently a student from <b> Dr. Akhilesh Das Gupta Institute of Technology & Management</b> persuing B.tech. I'm a creative problem solving enthusiast and love heart building Websites.
+         Currently a student from <b> Dr. Akhilesh Das Gupta Institute of Technology & Management</b> persuing B.tech. I'm a creative problem solving enthusiast and love 💖 building Websites.
          </p>
          <p className="break-words last">
          Logical and results-driven Web Developer dedicated to build and optimize user-focused websites for customers with various business objectives.Judicious and creative when crafting effective websites, apps and platforms to propel competitive advantage and revenue growth. Technically proficient and analytical problem solver with a calm and focused demeanor. 
