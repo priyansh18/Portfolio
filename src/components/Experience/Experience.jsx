f807541cf5e8588272afa89b7dc10af803085f1a
priyansh18 <singhalpriyansh58@gmail.com>
2021-05-28 19:51:28 +0530

    New Experience Added & Resume Updated

--- src/components/Experience/Experience.jsx
+++ src/components/Experience/Experience.jsx
@@ -1,3 +1,4 @@
+
 import React from "react";
 import "./Experience.scss";
 
@@ -8,27 +9,52 @@ const Experience = () => {
         <h4>Experience</h4>
       </section>
       <div className="container">
-        <div className="left">
-          <div className="date">
-            <span>May-Jun 2020</span>
+        <div class="experiences">
+          <div className="left">
+            <div className="date">
+              <span>May-Jun 2020</span>
+            </div>
+            <div className="occupation">
+              <span> Web Developer </span>
+            </div>
           </div>
-          <div className="occupation">
-            <span> Web Developer </span>
+          <div className="right">
+            <div className="place">
+              <span>@</span>JDMR ISCHOOL
+            </div>
+            <div className="task-list">
+              <div className="heading">
+                Key responsibilities during the internship were -
+              </div>
+              <ul className="list">
+                <li>Changes in WebPage</li>
+                <li>Created Teaching board using P5.js </li>
+              </ul>
+            </div>
           </div>
         </div>
-        <div className="right">
-          <div className="place">
-            <span>@</span>JDMR ISCHOOL
-          </div>
-          <div className="task-list">
-            <div className="heading">
-              Key responsibilities during the internship (1 months) were -
+        <div class="experiences">
+          <div className="left">
+            <div className="date">
+              <span>April-May 2021</span>
             </div>
-            <ul className="list">
-              <li>Changes in WebPage</li>
-              <li>Created Teaching board using P5.js </li>
-              
-            </ul>
+            <div className="occupation">
+              <span> Web Developer </span>
+            </div>
+          </div>
+          <div className="right">
+            <div className="place">
+              <span>@</span>Friends
+            </div>
+            <div className="task-list">
+              <div className="heading">
+                Key responsibilities during the internship were -
+              </div>
+              <ul className="list">
+                <li>Designing of whole Webpage. </li>
+                <li>Handle website for all screens.</li>
+              </ul>
+            </div>
           </div>
         </div>
       </div>
